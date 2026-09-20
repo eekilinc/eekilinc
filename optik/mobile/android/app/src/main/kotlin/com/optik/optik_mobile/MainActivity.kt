7e4db054e2032a7278e9d250c2e1d4f9a37e27e8
eekilinc <ekilinc@mehmetakif.edu.tr>
2026-09-21 01:39:30 +0300

    revert: remove optik directory pushed by mistake

--- optik/mobile/android/app/src/main/kotlin/com/optik/optik_mobile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.optik.optik_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- optik/mobile/lib/core/config.dart
+++
@@ -1,6 +0,0 @@
-// API adresi: derleme anında --dart-define=API_URL=... ile ezilebilir.
-// Android emülatörden host makineye erişim için 10.0.2.2 kullanılır.
-const String apiBaseUrl = String.fromEnvironment(
-  'API_URL',
-  defaultValue: 'http://10.0.2.2/api',
-);
